27bd2a1ae8be4a1d8a6f2551c2b988138544c770
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-05-20 20:51:14 +0700

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -2,39 +2,47 @@ import math
 import cv2
 import numpy as np
 
-
 class EuclideanDistTracker:
-    def __init__(self):
-        # Store the center positions of the objects
+    def __init__(self, svm_model):
         self.center_points = {}
-        # Store the Kalman filters for object tracking
         self.kalman_filters = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
         self.id_count = 0
+        self.svm_model = svm_model
 
-    def update(self, objects_rect):
+
+    def detect_vehicle(self, img):
+        img = cv2.resize(img, (64, 128))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        hog = cv2.HOGDescriptor()
+        features = hog.compute(gray)
+        threshold = 0.7
+        self.svm_model.decision_function = lambda X: np.where(self.svm_model.decision_function(X) >= threshold, 1, -1)
+        return self.svm_model.predict([features])[0]
+
+    def update(self, objects_rect, frame):
         objects_bbs_ids = []
 
-        # Get center point of new object
         for rect in objects_rect:
             x, y, w, h = rect
             cx = x + (w // 2)
             cy = y + (h // 2)
 
-            # Find out if that object was detected already
             same_object_detected = False
             for object_id, center in self.center_points.items():
                 prev_cx, prev_cy = center
                 dist = math.hypot(cx - prev_cx, cy - prev_cy)
 
-                if dist < 50:
+                if dist < 200:
                     self.center_points[object_id] = (cx, cy)
                     objects_bbs_ids.append([x, y, w, h, object_id])
                     same_object_detected = True
                     break
 
-                if not same_object_detected:
+            if not same_object_detected:
+                roi = frame[y:y+h, x:x+w]
+                is_vehicle = self.detect_vehicle(roi)
+
+                if is_vehicle:
                     self.center_points[self.id_count] = (cx, cy)
 
                     kalman_filter = cv2.KalmanFilter(4, 2)
@@ -49,44 +57,17 @@ class EuclideanDistTracker:
                                                               [0, 0, 1, 0],
                                                               [0, 0, 0, 1]], np.float32) * 0.03
 
-                # Initialize the Kalman filter state with the current center point
-                kalman_filter.statePre = np.array([[cx], [cy], [0], [0]], np.float32)
+                    # Initialize the Kalman filter state with the current center point
+                    kalman_filter.statePre = np.array([[cx], [cy], [0], [0]], np.float32)
 
-                self.kalman_filters[self.id_count] = kalman_filter
+                    self.kalman_filters[self.id_count] = kalman_filter
 
-                objects_bbs_ids.append([x, y, w, h, self.id_count])
-                self.id_count += 1
+                    objects_bbs_ids.append([x, y, w, h, self.id_count])
+                    self.id_count += 1
 
-        # Clean the dictionary by removing IDs that are not used anymore
         self.center_points = {
             object_id: center
             for object_id, center in self.center_points.items()
             if object_id in [obj_bb_id[4] for obj_bb_id in objects_bbs_ids]
         }
-
-        # # Perform motion prediction using optical flow
-        # prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # curr_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # flow = cv2.calcOpticalFlowFarneback(prev_gray, curr_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        #
-        # for obj_bb_id in objects_bbs_ids:
-        #     if len(obj_bb_id) == 5:
-        #         x, y, w, h, object_id = obj_bb_id
-        #     elif len(obj_bb_id) == 7:
-        #         x, y, w, h, object_id, predicted_cx, predicted_cy = obj_bb_id
-        #     # Predict the new object position using optical flow motion vectors
-        #     dx = int(np.mean(flow[y:y + h, x:x + w, 0]))
-        #     dy = int(np.mean(flow[y:y + h, x:x + w, 1]))
-        #     predicted_cx = cx + dx
-        #     predicted_cy = cy + dy
-        #
-        #     # Update the Kalman filter with the predicted position
-        #     kalman_filter = self.kalman_filters[object_id]
-        #     kalman_filter.correct(np.array([[predicted_cx], [predicted_cy]], np.float32))
-        #     predicted_state = kalman_filter.predict()
-        #
-        #     objects_bbs_ids.append([x, y, w, h, object_id, predicted_state[0, 0], predicted_state[1, 0]])
-        # Update the Kalman filter for existing objects using optical flow
-
-
         return objects_bbs_ids
