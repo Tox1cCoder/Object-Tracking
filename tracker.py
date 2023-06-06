5cd504c0d384999cff0b1a42aa84fbc851c02773
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-06-06 20:47:10 +0700

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -4,47 +4,7 @@ import cv2
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-
-def non_max_suppression(boxes, overlap_threshold):
-    if len(boxes) == 0:
-        return []
-
-    boxes = np.array(boxes)
-    x1 = boxes[:, 0]
-    y1 = boxes[:, 1]
-    x2 = boxes[:, 0] + boxes[:, 2]
-    y2 = boxes[:, 1] + boxes[:, 3]
-    areas = boxes[:, 2] * boxes[:, 3]
-    picked_indices = []
-
-    for i in range(len(boxes)):
-        indices = list(range(i + 1, len(boxes)))
-        curr_x1 = x1[i]
-        curr_y1 = y1[i]
-        curr_x2 = x2[i]
-        curr_y2 = y2[i]
-        curr_area = areas[i]
-        for j in indices:
-            comp_x1 = x1[j]
-            comp_y1 = y1[j]
-            comp_x2 = x2[j]
-            comp_y2 = y2[j]
-            comp_area = areas[j]
-
-            int_x1 = max(curr_x1, comp_x1)
-            int_y1 = max(curr_y1, comp_y1)
-            int_x2 = min(curr_x2, comp_x2)
-            int_y2 = min(curr_y2, comp_y2)
-
-            int_area = max(0, int_x2 - int_x1 + 1) * max(0, int_y2 - int_y1 + 1)
-            iou = int_area / (curr_area + comp_area - int_area)
-
-            if iou < overlap_threshold:
-                picked_indices.append(j)
-
-    picked_boxes = [box for i, box in enumerate(boxes) if i not in picked_indices]
-    return picked_boxes
-
+object_id = 0
 
 class EuclideanDistTracker:
     def __init__(self, svm_model):
@@ -62,6 +22,8 @@ class EuclideanDistTracker:
 
     def update(self, detections, frame):
 
+        global object_id  # Access the global object ID variable
+
         if len(self.tracks) == 0:
             for det in detections:
                 x, y, w, h = det
@@ -73,11 +35,13 @@ class EuclideanDistTracker:
 
                 if is_vehicle:
                     self.tracks.append({
-                        'id': len(self.tracks),
+                        'id': object_id,  # Assign the current object ID
                         'bbox': (x, y, w, h),
                         'centroid': (cx, cy),
                         'age': 1,
                     })
+                    object_id += 1  # Increment the object ID
+
         else:
             # Create detection matrix
             detection_matrix = np.zeros((len(self.tracks), len(detections)))
@@ -95,7 +59,7 @@ class EuclideanDistTracker:
             for i, track in enumerate(self.tracks):
                 if i not in row_ind:
                     track['age'] += 1
-                    if track['age'] > 5:  # Remove tracks that are not detected for a certain number of frames
+                    if track['age'] > 10:  # Remove tracks that are not detected for a certain number of frames
                         self.tracks.remove(track)
 
             # Create new tracks for unmatched detections
@@ -110,11 +74,12 @@ class EuclideanDistTracker:
 
                     if is_vehicle:
                         self.tracks.append({
-                            'id': len(self.tracks),
+                            'id': object_id,  # Assign the current object ID
                             'bbox': (x, y, w, h),
                             'centroid': (cx, cy),
                             'age': 1,
                         })
+                        object_id += 1  # Increment the object ID
 
             # Update existing tracks with matched detections
             for i, j in zip(row_ind, col_ind):
@@ -125,24 +90,6 @@ class EuclideanDistTracker:
                     self.tracks[i]['bbox'] = (x, y, w, h)
                     self.tracks[i]['centroid'] = (cx, cy)
                     self.tracks[i]['age'] = 1
-
-            # Create new tracks for unmatched detections
-            unmatched_indices = set(range(len(detections))).difference(col_ind)
-            for j in unmatched_indices:
-                x, y, w, h = detections[j]
-                cx = x + (w // 2)
-                cy = y + (h // 2)
-
-                roi = frame[y:y + h, x:x + w]
-                is_vehicle = self.detect_vehicle(roi)
-
-                if is_vehicle:
-                    self.tracks.append({
-                        'id': len(self.tracks),
-                        'bbox': (x, y, w, h),
-                        'centroid': (cx, cy),
-                        'age': 1,
-                    })
 
         final_objects_bbs_ids = [[int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]), track['id']]
                                  for track in self.tracks
